4ab7a04c8f757c71354d4f6bb3d24ed6b19936f6
galaxy4276 <galaxyhi4276@gmail.com>
2020-09-14 22:12:30 +0900

    0914/[update]/기존 프로젝트에서 코드 이동

---
+++ src/components/Blue.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Blue.css';
+
+const Blue = () => {
+  return (
+    <div className="Blue">
+      Blue
+    </div>
+  );
+};
+
+
+export default Blue;
---
+++ src/components/Red.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/Red.css';
+
+const Red = () => {
+  return (
+    <div className="Red">
+      Red
+    </div>
+  );
+};
+
+
+export default Red;
